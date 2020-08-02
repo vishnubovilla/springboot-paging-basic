0a2d05fa362d61b457724464871e919fc364d98d
Vishnu Bovilla <vbovilla@dminc.com>
2020-08-02 18:27:44 +0530

    initial setup for pagination using spring boot.

---
+++ src/main/java/in/dminc/springboot/paging/SpringbootPagingBasicApplication.java
@@ -0,0 +1,13 @@
+package in.dminc.springboot.paging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPagingBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPagingBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dminc/springboot/paging/SpringbootPagingBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dminc.springboot.paging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPagingBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
